2873e1558edc326e881c8968dfba0c60db56a89e
Yamoon224 <Yamooon664@gmail.com>
2025-08-27 13:19:06 +0000

    new file:   public/images/favicon.png 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/layouts/auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -16,9 +16,9 @@
         <meta property="og:type" content="website">
         <meta property="og:url" content="">
         <meta property="og:image" content="">
-        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('media/favicons/favicon-192x192.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+        <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('images/favicon.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon.png') }}">
         <link rel="stylesheet" id="css-main" href="{{ asset('css/oneui.min-5.11.css') }}">
         <script src="{{ asset('js/setTheme.js') }}"></script>
 
