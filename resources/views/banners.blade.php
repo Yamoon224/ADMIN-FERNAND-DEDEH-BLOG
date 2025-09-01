1661151990001cf2ad677bd56be4935ca424b99e
Yamoon224 <Yamooon664@gmail.com>
2025-09-01 14:17:05 +0000

    modified:   resources/views/banners.blade.php

--- resources/views/banners.blade.php
+++ resources/views/banners.blade.php
@@ -121,51 +121,37 @@
                     aria-labelledby="sidebar_banners-tab" tabindex="0">
                     <div class="row">
                         @foreach ($verticales as $item)
-                        <div class="col-md-6 col-xl-4">
-                            <a class="block block-rounded bg-image h-100 mb-0"
-                               style="background-image: url('{{ asset($item->image_path) }}');"
-                               href="javascript:void(0)">
-                                <div class="block-content bg-black-50">
-                                    <div class="mb-5 mb-sm-7 d-flex justify-content-between align-items-center">
-                                        <!-- Badge position à gauche -->
-                                        <span class="badge bg-primary fw-bold p-2 text-uppercase">{{ $item->position }}</span>
-                        
-                                        <!-- Boutons à droite -->
-                                        <div class="d-flex gap-2">
-                                            <a 
-                                                role="button"
-                                                class="btn btn-sm btn-primary"
-                                                data-id="{{ $item->id }}"
-                                                data-link="{{ $item->link }}"
-                                                data-position="{{ $item->position }}"
-                                                data-bs-toggle="modal" 
-                                                data-bs-target="#edit-banner"
-                                                onclick="openEditbannerModal(this)"
-                                            >
-                                                <i class="si si-note me-1"></i>
-                                            </a>
-                        
-                                            <form action="{{ route('banners.destroy', $item->id) }}" method="post"
-                                                onsubmit="return confirm('@lang('locale.confirm_delete')')">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="btn btn-sm btn-danger">
-                                                    <i class="si si-trash me-1"></i>
-                                                </button>
-                                            </form>
+                            <div class="col-md-6 col-xl-4">
+                                <a class="block block-rounded bg-image h-100 mb-0"
+                                    style="background-image: url('{{ asset($item->image_path) }}');"
+                                    href="javascript:void(0)">
+                                    <div class="block-content bg-black-50">
+                                        <div
+                                            class="mb-5 mb-sm-7 d-sm-flex justify-content-sm-between align-items-sm-center">
+                                            <p>
+                                                <span
+                                                    class="badge bg-primary fw-bold p-2 text-uppercase">{{ $item->position }}</span>
+                                            </p>
+                                            <p class="fs-sm">
+                                                <span class="text-white fw-semibold me-1">
+                                                    <i class="fa fa-fw fa-eye text-white-50"></i> 400
+                                                </span>
+                                                <span class="text-white fw-semibold me-1">
+                                                    <i class="fa fa-fw fa-heart text-white-50"></i> 89
+                                                </span>
+                                                <span class="text-white fw-semibold me-1">
+                                                    <i class="fa fa-fw fa-comments text-white-50"></i> 44
+                                                </span>
+                                            </p>
                                         </div>
+                                        <p class="fs-lg fw-bold text-white mb-0">
+                                            Exploring the forest in the mist
+                                        </p>
+                                        <p class="fw-medium text-white-75"><a
+                                                href="{{ $item->link }}">@lang('locale.link')</a></p>
                                     </div>
-                        
-                                    <p class="fs-lg fw-bold text-white mb-0">
-                                        Exploring the forest in the mist
-                                    </p>
-                                    <p class="fw-medium text-white-75">
-                                        <a href="{{ $item->link }}">@lang('locale.link')</a>
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
-                        
+                                </a>
+                            </div>
                         @endforeach
                     </div>
 
