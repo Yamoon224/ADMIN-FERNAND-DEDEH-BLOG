51a94c4e2952f64539ad43167231e8c9cd4826e4
Yamoon224 <Yamooon664@gmail.com>
2025-09-01 10:38:18 +0000

    modified:   resources/views/dailies/add.blade.php

--- resources/views/dailies/add.blade.php
+++ resources/views/dailies/add.blade.php
@@ -46,7 +46,7 @@
                                 <div class="block-header block-header-default">
                                     <h3 class="block-title">@lang('locale.content', ['suffix'=>''])</h3>
                                 </div>
-                                <div class="block-content block-content-full" style="box-shadow: 0 4px 10px rgba(0,0,0,0.1); border-radius: 0.5rem;">
+                                <div class="block-content block-content-full border-bottom border-warning mb-4" style="border-bottom-width:2px; box-shadow: 0 4px 10px rgba(0,0,0,0.1); border-radius: 0.5rem;">
                                     <div class="hashtag-row" data-index="0">
                                         <div class="row align-items-start">
                                             <div class="col-lg-6 col-md-6">
